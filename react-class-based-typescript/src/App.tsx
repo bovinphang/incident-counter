de6a431a29879af0537c7f15e0df7be9325c8b42
pengbaowen <pengbaowen@richinfo.cn>
2022-06-12 20:24:44 +0800

    refactor: 增加生命周期注释

--- react-class-based-typescript/src/App.tsx
+++ react-class-based-typescript/src/App.tsx
@@ -7,12 +7,31 @@ type CounterProps = {
 
 type CounterState = {
     count: number;
+    hasError?: boolean;
 };
 
 class Counter extends Component<CounterProps, CounterState> {
+    // 生命周期(按照它们被调用的顺序):
+    // 1. 在 React 组件挂载之前，会调用它的构造函数。如果不初始化 state 或不进行方法绑定，则不需要为 React 组件实现构造函数。
+    constructor(props: CounterProps) {
+        super(props);
+        this.state = { count: 0, hasError: false };
+    }
+
+    /*    
+    // state的另一种初始化方法
     state: CounterState = {
         count: 0
-    };
+    }; 
+    */
+
+    // 2. 在调用 render 方法之前调用，并且在初始挂载及后续更新时都会被调用。它应返回一个对象来更新 state，如果返回 null 则不更新任何内容。此方法适用于罕见的用例。【不常用的生命周期】
+    static getDerivedStateFromProps(props, state) {}
+
+    // 3. 根据 shouldComponentUpdate() 的返回值，判断 React 组件的输出是否受当前 state 或 props 更改的影响。默认行为是 state 每次发生变化组件都会重新渲染。【不常用的生命周期】
+    shouldComponentUpdate(nextProps, nextState) {
+        return true;
+    }
 
     increment = () => this.setState(({ count }) => ({ count: count + 1 }));
     decrement = () => this.setState(({ count }) => ({ count: count - 1 }));
@@ -20,6 +39,7 @@ class Counter extends Component<CounterProps, CounterState> {
     changeCount = (event: ChangeEvent<HTMLInputElement>) =>
         this.setState({ count: +event.target.value });
 
+    // 4. 在组件挂载开始之前或props 和 state改变时被调用。如果 shouldComponentUpdate() 返回 false，则不会调用 render()。
     render() {
         const { incident } = this.props;
         const { count } = this.state;
@@ -51,6 +71,31 @@ class Counter extends Component<CounterProps, CounterState> {
             </main>
         );
     }
+
+    // 5. 在组件挂载后（插入 DOM 树中）立即调用。依赖于 DOM 节点的初始化应该放在这里。如需通过网络请求获取数据，此处是实例化请求的好地方。
+    componentDidMount() {}
+
+    // 6. 在最近一次渲染输出（提交到 DOM 节点）之前调用。它使得组件能在发生更改之前从 DOM 中捕获一些信息（例如，滚动位置）。此生命周期方法的任何返回值将作为参数传递给 componentDidUpdate()。应返回 snapshot 的值（或 null）。【不常用的生命周期】
+    getSnapshotBeforeUpdate(prevProps, prevState) {
+        return null;
+    }
+
+    // 7. 在状态更新后会被立即调用。首次渲染不会执行此方法。
+    componentDidUpdate(prevProps, prevState, snapshot) {}
+
+    // 8. 在组件卸载及销毁之前直接调用。在此方法中执行必要的清理操作，例如，清除 timer，取消网络请求或清除在 componentDidMount() 中创建的订阅等。
+    componentWillUnmount() {}
+
+    // 9. 此生命周期会在后代组件抛出错误后被调用。 它将抛出的错误作为参数，并返回一个值以更新 state
+    static getDerivedStateFromError(error) {
+        // 更新 state 使下一次渲染可以显降级 UI
+        return { hasError: true };
+    }
+
+    // 10. 此生命周期在后代组件抛出错误后被调用。它接收两个参数：
+    // 1) error —— 抛出的错误。
+    // 2) info —— 带有 componentStack key 的对象，其中包含有关组件引发错误的栈信息。
+    componentDidCatch(error, info) {}
 }
 
 class App extends Component {
--- vue2/src/App.vue
+++ vue2/src/App.vue
@@ -15,16 +15,37 @@ export default {
             console.log('The new counter value is: ' + val);
         }
     },
-    //生命周期钩子 (按照它们被调用的顺序):
+
+    // 生命周期钩子 (按照它们被调用的顺序):
+
+    // 1. 在实例初始化之后,进行数据侦听和事件/侦听器的配置之前同步调用。
     beforeCreate() {},
+
+    // 2. 在实例创建完成后被立即同步调用。
     created() {},
+
+    // 3. 在挂载开始之前被调用：相关的 render 函数首次被调用。
     beforeMount() {},
+
+    // 4. 实例被挂载后调用，这时 el 被新创建的 vm.$el 替换了。
     mounted() {},
+
+    // 5. 在数据发生改变后，DOM 被更新之前被调用。
     beforeUpdate() {},
+
+    // 6. 在数据更改导致的虚拟 DOM 重新渲染和更新完毕之后被调用。
     updated() {},
+
+    // 7. 被 keep-alive 缓存的组件激活时调用。
     activated() {},
+
+    // 8. 被 keep-alive 缓存的组件失活时调用。
     deactivated() {},
+
+    // 9. 实例销毁之前调用。
     beforeDestroy() {},
+
+    // 10. 实例销毁后调用。
     destroyed() {},
     // 方法
     methods: {
