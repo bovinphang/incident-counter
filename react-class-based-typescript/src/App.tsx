c421c47485062d50179f83d5a8f2d27acc667c67
pengbaowen <pengbaowen@richinfo.cn>
2022-06-12 22:44:25 +0800

    fix: 增加typescript类型声明

--- react-class-based-typescript/src/App.tsx
+++ react-class-based-typescript/src/App.tsx
@@ -10,6 +10,13 @@ type CounterState = {
     hasError?: boolean;
 };
 
+interface ErrorInfo {
+    /**
+     * Captures which component contained the exception, and its ancestors.
+     */
+    componentStack: string;
+}
+
 class Counter extends Component<CounterProps, CounterState> {
     // 生命周期(按照它们被调用的顺序):
     // 1. 在 React 组件挂载之前，会调用它的构造函数。如果不初始化 state 或不进行方法绑定，则不需要为 React 组件实现构造函数。
@@ -26,10 +33,12 @@ class Counter extends Component<CounterProps, CounterState> {
     */
 
     // 2. 在调用 render 方法之前调用，并且在初始挂载及后续更新时都会被调用。它应返回一个对象来更新 state，如果返回 null 则不更新任何内容。此方法适用于罕见的用例。【不常用的生命周期】
-    static getDerivedStateFromProps(props, state) {}
+    static getDerivedStateFromProps(props: CounterProps, state: CounterState) {
+        return null;
+    }
 
     // 3. 根据 shouldComponentUpdate() 的返回值，判断 React 组件的输出是否受当前 state 或 props 更改的影响。默认行为是 state 每次发生变化组件都会重新渲染。【不常用的生命周期】
-    shouldComponentUpdate(nextProps, nextState) {
+    shouldComponentUpdate(nextProps: CounterProps, nextState: CounterState) {
         return true;
     }
 
@@ -73,21 +82,25 @@ class Counter extends Component<CounterProps, CounterState> {
     }
 
     // 5. 在组件挂载后（插入 DOM 树中）立即调用。依赖于 DOM 节点的初始化应该放在这里。如需通过网络请求获取数据，此处是实例化请求的好地方。
-    componentDidMount() {}
+    componentDidMount(): void {}
 
     // 6. 在最近一次渲染输出（提交到 DOM 节点）之前调用。它使得组件能在发生更改之前从 DOM 中捕获一些信息（例如，滚动位置）。此生命周期方法的任何返回值将作为参数传递给 componentDidUpdate()。应返回 snapshot 的值（或 null）。【不常用的生命周期】
-    getSnapshotBeforeUpdate(prevProps, prevState) {
+    getSnapshotBeforeUpdate(prevProps: CounterProps, prevState: CounterState) {
         return null;
     }
 
     // 7. 在状态更新后会被立即调用。首次渲染不会执行此方法。
-    componentDidUpdate(prevProps, prevState, snapshot) {}
+    componentDidUpdate(
+        prevProps: CounterProps,
+        prevState: CounterState,
+        snapshot: any
+    ): void {}
 
     // 8. 在组件卸载及销毁之前直接调用。在此方法中执行必要的清理操作，例如，清除 timer，取消网络请求或清除在 componentDidMount() 中创建的订阅等。
-    componentWillUnmount() {}
+    componentWillUnmount(): void {}
 
     // 9. 此生命周期会在后代组件抛出错误后被调用。 它将抛出的错误作为参数，并返回一个值以更新 state
-    static getDerivedStateFromError(error) {
+    static getDerivedStateFromError(error: Error) {
         // 更新 state 使下一次渲染可以显降级 UI
         return { hasError: true };
     }
@@ -95,7 +108,7 @@ class Counter extends Component<CounterProps, CounterState> {
     // 10. 此生命周期在后代组件抛出错误后被调用。它接收两个参数：
     // 1) error —— 抛出的错误。
     // 2) info —— 带有 componentStack key 的对象，其中包含有关组件引发错误的栈信息。
-    componentDidCatch(error, info) {}
+    componentDidCatch(error: Error, errorInfo: ErrorInfo) {}
 }
 
 class App extends Component {
